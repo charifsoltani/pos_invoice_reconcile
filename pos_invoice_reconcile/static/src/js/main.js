f94bf2ecfbd43b6de58136bd6b2ca5d841169280
Soltani charif <soltani.charif@gmail.com>
2018-01-26 17:35:13 +0100

    Fix js file remove refound support just keep the partial payment

--- pos_invoice_reconcile/static/src/js/main.js
+++ pos_invoice_reconcile/static/src/js/main.js
@@ -1,7 +1,9 @@
 
-openerp.pos_partial_payment = function(instance) {
+openerp.pos_invoice_reconcile = function(instance) {
+    console.info('rani hna');
 
      var module = instance.point_of_sale;
+
 
      // update the screen a little bit to enable invoice when the user didn't save payment
      openerp_pos_partial_payment_screens_update(instance, module);
